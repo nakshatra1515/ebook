08eeaccc14f036fe07cf921813ae9e32d0286d6c
nakshatra1515 <nakshatra863068@gmail.com>
2023-03-27 10:31:15 +0530

    initial commit

---
+++ Ebook-App/src/main/java/com/DAO/UserDAOImp.java
@@ -0,0 +1,43 @@
+package com.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.entity.User;
+
+public class UserDAOImp implements UserDAO{
+
+	private Connection conn;
+
+	public UserDAOImp(Connection conn) {
+		super();
+		this.conn = conn;
+	}
+
+	public boolean userRegister(User us) {
+		@SuppressWarnings("unused")
+		boolean f=false;
+		try {
+			String sql="insert into user(name,email,phno,password) values(?,?,?,?)";
+			PreparedStatement ps=conn.prepareStatement(sql);
+			ps.setString(1, us.getName());
+			ps.setString(2, us.getEmail());
+			ps.setString(3, us.getPhno());
+			ps.setString(4, us.getPassword());
+			
+			int i=ps.executeUpdate();
+			if(i==1) {
+				f=true;
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+}
